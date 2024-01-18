85228198be2eccc58a0c96a2f7ec60638e056c41
@s.kriuchkov <s.kriuchkov@stc-spb.ru>
2024-01-18 22:50:53 +0300

    [+] another three tests

--- main.cpp
+++ main.cpp
@@ -13,6 +13,31 @@ TEST(SOLVER_TEST, NO_ROOTS)
     EXPECT_TRUE(Solver::Solve(a, b, c).empty());
 }
 
+TEST(SOLVER_TEST, TWO_ROOTS)
+{
+    double a = 1.0, b = 0.0, c = -1.0;
+    double first_root = 1.0, second_root = -1.0;
+    auto roots = Solver::Solve(a, b, c);
+    EXPECT_EQ(roots.size(), 2);
+    EXPECT_DOUBLE_EQ(roots[0], first_root);
+    EXPECT_DOUBLE_EQ(roots[1], second_root);
+}
+
+TEST(SOLVER_TEST, ONLY_ROOT)
+{
+    double a = 1e-5, b = 2e-5, c = 1e-7;
+    double root = -1.0;
+    auto roots = Solver::Solve(a, b, c);
+    EXPECT_EQ(roots.size(), 1);
+    EXPECT_DOUBLE_EQ(roots[0], root);
+}
+
+TEST(SOLVER_TEST, NAN_CHECK)
+{
+    double a = 0.0/0.0;
+    EXPECT_ANY_THROW(Solver::Solve(a));
+}
+
 int main(int argc, char* argv[])
 {
     testing::InitGoogleTest(&argc, argv);
--- solver.cpp
+++ solver.cpp
@@ -1,17 +1,34 @@
 #include "solver.h"
+#include <math.h>
 
 const double Solver::e = 1e-5;
 
 std::vector<double> Solver::Solve(double a, double b, double c)
 {
     std::vector<double> result;
-    if(a < e)
+    if(abs(a) < e)
+        throw std::exception();
+    if(isnan(a) || isnan(b) || isnan(c) ||
+       isinf(a) || isinf(b) || isinf(c))
         throw std::exception();
 
     double d = b*b - 4*a*c;
 
     if(d < 0.0)
         return result;
+    else if(abs(d) < e) // равно 0
+    {
+        double root = -b/(2*a);
+        result.push_back(root);
+    }
+    else if(d > 0.0)
+    {
+        double tmp = sqrt(d)/(2*a);
+        double root = -b + tmp;
+        result.push_back(root);
+        root = -b - tmp;
+        result.push_back(root);
+    }
     
     return result;
 }
